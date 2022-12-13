f894cd403e07cddb0dae1fad97e7a499f8e03f4f
Leago Thulare <lsthulare@gmail.com>
2022-12-13 13:09:07 -0800

    2nd try

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,14 +8,14 @@
 
 int main(void)
 {
-		int i = 0;
-			char name_of_school[] = "_putchar";
+	int i = 0;
+	char name_of_school[] = "_putchar";
 
-				while (i < 9)
-						{
-									_putchar(name_of_school[i]);
-											i++;
-												}
-					_putchar('\n');
-						return (0);
+	while (i < 9)
+	{
+		_putchar(name_of_school[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
 }
--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,8 +1,1 @@
-#include <stdio.h>
 
-int main(void)
-{
-		print_alphabet_x10();
-		       	print_alphabet();
-				return(0);
-}
