671653a0316074adf5bef3876f4afd7fdae063c9
Leago Thulare <lsthulare@gmail.com>
2022-12-15 10:45:39 -0800

    Nested Loops

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *  *  * _isdigit -function checks for a digit 0-9
+ *   *   * @c:Checker
+ *    *    * Return: 1 if c is a digit, 0 is returned
+*/
+int _isdigit(int c)
+{
+	if ((c >= '0') && (c <= '9'))
+		return (1);
+else
+		return (0);
+}
