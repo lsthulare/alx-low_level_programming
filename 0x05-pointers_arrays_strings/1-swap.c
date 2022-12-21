93dfd935e7b035546a018cbce9db26b59ff39654
Leago Thulare <lsthulare@gmail.com>
2022-12-20 16:40:18 -0800

    Arrays

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,14 +2,15 @@
 
 /*
  * * swap - swaps the value of int a and int b
+ * * Owned by user
  * * @a: 1st int
  * * @b: 2nd int
+ * * return 0
  * * 
 */
 
 void swap_int(int *a, int *b)
 {
-
 	int s;
 
 	s = *a;
