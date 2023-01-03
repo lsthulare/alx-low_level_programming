782308a144ed7ca824fdca511d9ae3f5190e857b
lsthulare <118156447+lsthulare@users.noreply.github.com>
2023-01-03 02:48:14 +0200

    c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- *  Prints every minute of the day of
- *  starting from 00:00 to 23:59.
+ * jack_bauer - Prints every minute of the day of
+ *              Jack Bauer, starting from 00:00 to 23:59.
 */
 
 void jack_bauer(void)
