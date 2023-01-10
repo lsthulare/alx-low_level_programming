702cac90d2bc2247ef63103646b6beb5039c8947
Leago Thulare <lsthulare@gmail.com>
2023-01-10 15:09:37 -0800

    malloc

--- 0x0B-malloc_free/4-free-grid.c
+++ 0x0B-malloc_free/4-free-grid.c
@@ -5,17 +5,16 @@
  *  * free_grid - frees a 2D board previously created by alloc_grid function.
  *   * @grid: the board to freed
  *    * @height: height of grid
- *     *
  *      * Return: void pointer
 */
 
 void free_grid(int **grid, int height)
 {
-		int index;
+	int index;
 
-			if (board == NULL || height <= 0)
-						return;
-				for (index = 0; index < height; index++)
-							free(board[index]);
-					free(board);
+	if (grid == NULL || height <= 0)
+		return;
+	for (index = 0; index < height; index++)
+		free(grid[index]);
+	free(grid);
 }
