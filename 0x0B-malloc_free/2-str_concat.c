b5334fa3076df9bc7b2fa62a934ec2e206ded7d7
Leago Thulare <lsthulare@gmail.com>
2023-01-10 16:21:59 -0800

    malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,10 +16,10 @@ char *str_concat(char *s1, char *s2)
 	int i, count = 0, len = 0;
 
 	if (s1 == 0)
-		s1 = '\0';
+		s1 = "";
 
 	if (s2 == 0)
-		s2 = '\0';
+		s2 = "";
 
 	for (i = 0; s1[i] || s2[i]; i++)
 			len++;
