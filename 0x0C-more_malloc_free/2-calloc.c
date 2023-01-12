739c9ae2da10252a66fc0cb92fb839d85ef5c759
Leago Thulare <lsthulare@gmail.com>
2023-01-12 15:31:06 -0800

    Malloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,7 +12,7 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *arr;
-	char *rplc;
+	//char *rplc;
 	unsigned int index;
 
 	if (nmemb == 0 || size == 0)
@@ -26,9 +26,9 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	{
 		return (NULL);
 	}
-	rplc =arr;
+	//rplc =arr;
 	for (index = 0; index < (size * nmemb); index++)
 		rplc[index] = '\0';
 
-	return (arr);
+	return (rplc);
 }
