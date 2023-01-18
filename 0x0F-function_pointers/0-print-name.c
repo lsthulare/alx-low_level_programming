debb93bb399902d1bfda9da73e856cc4bc47289c
Leago Thulare <lsthulare@gmail.com>
2023-01-18 13:05:00 -0800

    Pointers

--- 0x0F-function_pointers/0-print-name.c
+++ 0x0F-function_pointers/0-print-name.c
@@ -6,8 +6,11 @@
  *    * @f: A pointer to a function that prints a name.
 */
 
-void print_name(char *name, void (*f)(char *)) {
+void print_name(char *name, void (*f)(char *))
+{
 	if (!name || !f)
+	{
 		return;
+	}
 	f(name);
 }
