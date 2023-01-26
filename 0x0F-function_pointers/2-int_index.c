7b9985ba79fee9d61ecf593ba41b6c5cd1e0535f
lsthulare <118156447+lsthulare@users.noreply.github.com>
2023-01-26 03:13:41 +0200

    pointer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,9 +11,21 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-    if (!array || !cmp) return -1;
-    for (int i = 0; i < size; i++)
-        if (cmp(array[i])) return i;
-    return -1;
-}
+	int i = 0;
 
+	if (size > 0)
+	{
+		if (array != NULL && cmp != NULL)
+		{
+			while (i < size)
+			{
+				if (cmp(array[i]))
+					return (i);
+
+				i++;
+			}
+		}
+	}
+
+	return (-1);
+}
