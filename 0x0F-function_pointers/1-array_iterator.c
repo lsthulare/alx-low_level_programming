30ba73e14bf2e9ee1331644717d303f3096525e8
lsthulare <118156447+lsthulare@users.noreply.github.com>
2023-01-26 03:30:48 +0200

    pointer

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -8,9 +8,14 @@
  *      * @action: A pointer to the function to be executed.
 */
 
-void array_iterator(const int* array, const size_t size, void (*action)(int))
+void array_iterator(int *array, size_t size, void (*action)(int))
 {
-    if (!array || !action) return;
-    for (size_t i = 0; i < size; i++)
-        action(array[i]);
+	if (array == NULL || action == NULL)
+		return;
+
+	while (size-- > 0)
+	{
+		action(*array);
+		array++;
+	}
 }
